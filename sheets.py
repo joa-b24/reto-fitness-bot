78232abf6e4de9371f3d2cb31b1ab8d2ed425f02
Joana Itzel Barreto López <161401443+joa-b24@users.noreply.github.com>
2025-11-01 22:48:13 +0100

    Simplify get_sheet function by using existing client
    Refactored to use a pre-existing client for accessing sheets.

--- sheets.py
+++ sheets.py
@@ -1,15 +1,6 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from config import GOOGLE_CREDENTIALS, SHEET_NAME, materialize_service_account
-
-def get_client():
-
-    materialize_service_account()
-    scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_CREDENTIALS, scope)
-    return gspread.authorize(creds)
+from config import client, SHEET_NAME
 
 def get_sheet(sheet_name):
-    client = get_client()
     return client.open(SHEET_NAME).worksheet(sheet_name)
+
 
